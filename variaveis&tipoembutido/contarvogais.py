b4be196abf522a105cbdbc99f9c6ce916a768642
Lara <Laraewellen@gmail.com>
2025-03-21 10:20:06 -0300

    contando vogais

---
+++ variaveis&tipoembutido/contarvogais.py
@@ -0,0 +1,8 @@
+frase = input("Digite uma frase: ").upper() #convertendo tudo para maiúsculo
+
+print(f"Na frase {frase}" "tem:\n",
+    f"Qtd de A: {frase.count("A")}\n",
+    f"Qtd de A: {frase.count("E")}\n",
+    f"Qtd de A: {frase.count("I")}\n",
+    f"Qtd de A: {frase.count("O")}\n",
+    f"Qtd de A: {frase.count("U")}\n")
